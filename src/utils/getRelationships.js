0351de84a235225b2a4fa1f74254c554fc98a6e0
Dylan <dylanslack@gmail.com>
2016-09-21 11:29:43 -0600

    Remove valid relationship check
    This logic should be moved to the schema level. Bump version to 1.0.0-alpha.8.

--- src/utils/getRelationships.js
+++ src/utils/getRelationships.js
@@ -2,17 +2,6 @@ import { forEach } from 'lodash';
 import invalidSchemaType from '../errors/invalidSchemaType';
 
 const { keys } = Object;
-const allowedTypes = ['hasMany', 'hasOne', 'belongsTo'];
-
-const throwIfInvalidRelationship = (object) => {
-  if (keys(object).length !== 2) {
-    throw new Error('Incorrect number of properties, expecting 2.');
-  }
-
-  if (!allowedTypes.includes(keys(object)[0])) {
-    throw new Error('Invalid relationship type, expecting `hasMany`, `belongsTo`, or `hasOne`.');
-  }
-};
 
 /**
  * Return all original and inverse relationships of a given table.
@@ -45,8 +34,6 @@ export default (schemas, originalTable) => {
   forEach(schemas[originalTable].relationships, (obj, key) => {
     if (obj.hasOwnProperty('embedded')) return;
 
-    throwIfInvalidRelationship(obj);
-
     const field = key;
     const original = keys(obj)[0];
     const table = obj[original];
@@ -54,8 +41,6 @@ export default (schemas, originalTable) => {
 
     forEach(schemas[table].relationships, subObj => {
       if (subObj.hasOwnProperty('embedded')) return;
-
-      throwIfInvalidRelationship(subObj);
 
       const inverse = keys(subObj)[0];
 
