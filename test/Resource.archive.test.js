247a9d6ce310074d7fbc0de9cf64a8345718f34f
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-10-17 08:17:50 -0600

    Expand archive `hasMany - belongsTo` test

--- test/Resource.archive.test.js
+++ test/Resource.archive.test.js
@@ -1,5 +1,4 @@
 import test from 'ava';
-import Resource from '../src/Resource';
 import applyHooks from './helpers/applyHooks';
 import { model } from '../src';
 
@@ -8,21 +7,24 @@ applyHooks(test);
 test('should archive hasMany - belongsTo (user - blog) relationship', async t => {
   try {
     const blogger = await model('user').fetchResource('1');
-    const blog = await model('blog').fetchResource('1');
-
-    t.truthy(blogger instanceof Resource);
-    t.truthy(blog instanceof Resource);
+    const blog1 = await model('blog').fetchResource('1');
+    const blog2 = await model('blog').fetchResource('2');
 
     t.false(blogger.meta._archived);
-    t.false(blog.meta._archived);
-    t.false(blog.relationship('author').record._archived);
+    t.false(blog1.meta._archived);
+    t.false(blog2.meta._archived);
+    t.false(blog1.relationship('author').record._archived);
+    t.false(blog2.relationship('author').record._archived);
 
     const deletedBlogger = await blogger.archive();
-    const defunctBlog = await model('blog').fetchResource('1');
+    const defunctBlog1 = await model('blog').fetchResource('1');
+    const defunctBlog2 = await model('blog').fetchResource('2');
 
     t.true(deletedBlogger.meta._archived);
-    t.true(defunctBlog.meta._archived);
-    t.true(defunctBlog.relationship('author').record._archived);
+    t.true(defunctBlog1.meta._archived);
+    t.true(defunctBlog2.meta._archived);
+    t.true(defunctBlog1.relationship('author').record._archived);
+    t.true(defunctBlog2.relationship('author').record._archived);
   } catch (err) {
     t.fail(err.message);
   }
