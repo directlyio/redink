896d9bfa33ab9784cfb22e64f09b3d4a028384ff
calvinbrewer <brewercalvinj@gmail.com>
2016-11-11 11:43:14 -0700

    Fix `isHasOneValid` constraint

--- src/constraints/utils/isHasOneValid.js
+++ src/constraints/utils/isHasOneValid.js
@@ -1,12 +1,13 @@
 export default (record, field) => {
   const fieldToCheck = record[field];
 
+  // TODO: Create intensive docs on each check.
   if (!record) return false;
   if (!record._meta) return false;
   if (record._meta._archived) return false;
   if (!fieldToCheck) return true;
   if (fieldToCheck._archived) return true;
-  if (fieldToCheck._related) return true;
+  if (!fieldToCheck._related) return true;
 
   return false;
 };
